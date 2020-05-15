dda7722ab43a4f70d648576905a5d85760424e4f
sulemaez <steve.oyeyo@gmail.com>
2020-05-15 23:18:44 +0300

    intial rasbery code + init spring

---
+++ eds/src/main/java/com/eds/eds/EmergencyDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.eds.eds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyDetectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyDetectionSystemApplication.class, args);
+    }
+
+}
